4a257dc8ccc487e97bac48275937abfbe5df327a
Farzin khaledi <Khaledifarzin1@gmail.com>
2023-02-09 14:36:13 +0000

    change firebase config

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,29 +1,28 @@
-import { useEffect, useState } from "react";
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import Home from "./home";
-import Login from '../Login';
+// import { useEffect, useState } from "react";
+// import { getAuth, onAuthStateChanged } from 'firebase/auth';
+// import Home from "./home";
+// import Login from '../Login';
 
-const Dashboard = () => {
+// const Dashboard = () => {
 
-    const [user, setUser] = useState(null);
-    const auth = getAuth();
+//     const [user, setUser] = useState(null);
+//     const auth = getAuth();
 
-    useEffect(() => {
-        onAuthStateChanged(auth, (user) => {
-            if(user) {
-                setUser(user);
-            } else {
-                setUser(null);
-            }
-        })
-    }, []);
+//     useEffect(() => {
+//         onAuthStateChanged(auth, (user) => {
+//             if(user) {
+//                 setUser(user);
+//             } else {
+//                 setUser(null);
+//             }
+//         })
+//     }, []);
 
+//     return (
+//        <div>
+//            {user ? <Home /> : <Login />}
+//        </div>
+//     )
+// }
 
-    return (
-       <div>
-           {user ? <Home /> : <Login />}
-       </div>
-    )
-}
-
-export default Dashboard;
+// export default Dashboard;
--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,22 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-import { getStorage } from 'firebase/storage';
-import { getFirestore } from 'firebase/firestore/lite';
-const firebaseConfig= {
-    apiKey: 'AIzaSyDjJAuIR4SXYp90IcIP0BeDKZwLAzJirKg',
-    authDomain: 'my-website-5cd3e.firebaseapp.com',
-    projectId: 'my-website-5cd3e',
-    storageBucket: 'my-website-5cd3e.appspot.com',
-    messagingSenderId: '493637542163',
-    appId: '1:493637542163:web:55534fe544fcb4167b3ae6',
-    measurementId: 'G-SBYQ2R7L06',
-  };
+import { getFirestore } from '@firebase/firestore';
+// import { getAnalytics } from "firebase/analytics";
 
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+console.log(firebaseConfig.databaseURL);
+
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth();
-const provider = new GoogleAuthProvider();
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+// const analytics = getAnalytics(app);
+const db = getFirestore(app);
 
-export const signInWithGoogle = () => signInWithPopup(auth, provider);
+export default db;
+console.log(db);
